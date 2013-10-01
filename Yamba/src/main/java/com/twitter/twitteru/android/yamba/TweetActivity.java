d0f29ea79772d8720ac4a1d6db127c87da7e39e9
Ken Jones <kennethj@twitter.com>
2013-10-01 12:37:06 -0400

    Added logging for TweetActivity lifecycle methods

--- Yamba/src/main/java/com/twitter/twitteru/android/yamba/TweetActivity.java
+++ Yamba/src/main/java/com/twitter/twitteru/android/yamba/TweetActivity.java
@@ -2,14 +2,54 @@ package com.twitter.twitteru.android.yamba;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class TweetActivity extends Activity {
+    public static final String TAG = "TweetActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v(TAG, "onCreate() invoked");
+
         setContentView(R.layout.activity_tweet);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG, "onStart() invoked");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG, "onPause() invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG, "onResume() invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG, "onStop() invoked");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v(TAG, "onRestart() invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "onDestroy() invoked");
     }
 
 
@@ -19,5 +59,5 @@ public class TweetActivity extends Activity {
         getMenuInflater().inflate(R.menu.tweet, menu);
         return true;
     }
-    
+
 }
