ba6e751897d9ec294e9308a5d32903cfbed71541
Ken Jones <kennethj@twitter.com>
2013-10-02 17:20:45 -0400

    Began to refactor TweetActivity into a fragment implementation. Incomplete at the end of class Wednesday.

--- Yamba/src/main/java/com/twitter/twitteru/android/yamba/TimelineActivity.java
+++ Yamba/src/main/java/com/twitter/twitteru/android/yamba/TimelineActivity.java
@@ -3,10 +3,14 @@ package com.twitter.twitteru.android.yamba;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.app.LoaderManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.SimpleCursorAdapter;
 
 import com.twitter.twitteru.android.yamba.svc.YambaContract;
@@ -26,6 +30,7 @@ public class TimelineActivity extends ListActivity implements LoaderManager.Load
         android.R.id.text1,
         android.R.id.text2,
     };
+    private static final String TAG = "TimelineActivity";
 
     SimpleCursorAdapter mAdapter;
 
@@ -42,16 +47,43 @@ public class TimelineActivity extends ListActivity implements LoaderManager.Load
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new CursorLoader(this, YambaContract.Timeline.URI, null, null, null, null);
+        return new CursorLoader(
+                this,
+                YambaContract.Timeline.URI,
+                null,
+                null,
+                null,
+                YambaContract.Timeline.Columns.TIMESTAMP + " DESC");
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (BuildConfig.DEBUG) Log.v(TAG, "Cursor returned " + data.getCount() + " rows");
         mAdapter.swapCursor(data);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         mAdapter.swapCursor(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.timeline, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_tweet:
+                // Display the TweetActivity
+                startActivity(new Intent(this, TweetActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ Yamba/src/main/java/com/twitter/twitteru/android/yamba/TweetFragment.java
@@ -0,0 +1,10 @@
+package com.twitter.twitteru.android.yamba;
+
+import android.app.Fragment;
+
+/**
+ * Created by kennethj on 10/2/13.
+ */
+public class TweetFragment extends Fragment {
+
+}
