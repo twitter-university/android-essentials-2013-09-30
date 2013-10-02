1defce2ee78e36ac7b4f5369ff21df02d0ab1212
Ken Jones <kennethj@twitter.com>
2013-10-02 11:53:29 -0400

    Posting tweet via the YambaService

--- Yamba/src/main/java/com/twitter/twitteru/android/yamba/TweetActivity.java
+++ Yamba/src/main/java/com/twitter/twitteru/android/yamba/TweetActivity.java
@@ -1,55 +1,127 @@
 package com.twitter.twitteru.android.yamba;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class TweetActivity extends Activity {
+import com.twitter.twitteru.android.yamba.svc.YambaServiceHelper;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
+
+public class TweetActivity extends Activity implements View.OnClickListener {
     public static final String TAG = "TweetActivity";
+
+    EditText mEditTweet;
+    YambaClient mYambaClient;
+    Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.v(TAG, "onCreate() invoked");
+        if (BuildConfig.DEBUG) Log.v(TAG, "onCreate() invoked");
 
         setContentView(R.layout.activity_tweet);
+        mEditTweet = (EditText) findViewById(R.id.edit_tweet);
+        Button buttonPost = (Button) findViewById(R.id.button_post);
+        buttonPost.setOnClickListener(this);
+
+        // Initialize a reusable Toast
+        mToast = Toast.makeText(this, null, Toast.LENGTH_LONG);
+
+        mYambaClient = new YambaClient("student", "password");
     }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.button_post:
+                if (BuildConfig.DEBUG) Log.v(TAG, "Tweet button clicked");
+                postTweet();
+                break;
+            default:
+                // We should never get here!
+                Log.w(TAG, "Unknown view clicked?");
+        }
+    }
+
+    private void postTweet() {
+        String msg = mEditTweet.getText().toString();
+        mEditTweet.setText(null);
+        if (!TextUtils.isEmpty(msg)) {
+            if (BuildConfig.DEBUG) Log.v(TAG, "User entered: " + msg);
+            // Post the tweet on a worker thread using an AsyncTask
+            YambaServiceHelper.post(this, msg);
+        }
+    }
+
+    /* Old AsyncTask implementation
+    class PostToTwitter extends AsyncTask<String, Void, Integer> {
+        @Override
+        protected Integer doInBackground(String... params) {
+            int result = R.string.post_tweet_fail;
+            if (params.length > 0) {
+                try {
+                    mYambaClient.postStatus(params[0]);
+                    result = R.string.post_tweet_success;
+                } catch (YambaClientException e) {
+                    Log.e(TAG, "Failed to post tweet", e);
+                }
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            mToast.setText(result);
+            mToast.show();
+        }
+    }
+    */
+
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.v(TAG, "onStart() invoked");
+        if (BuildConfig.DEBUG) Log.v(TAG, "onStart() invoked");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.v(TAG, "onPause() invoked");
+        if (BuildConfig.DEBUG) Log.v(TAG, "onPause() invoked");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.v(TAG, "onResume() invoked");
+        if (BuildConfig.DEBUG) Log.v(TAG, "onResume() invoked");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.v(TAG, "onStop() invoked");
+        if (BuildConfig.DEBUG) Log.v(TAG, "onStop() invoked");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.v(TAG, "onRestart() invoked");
+        if (BuildConfig.DEBUG) Log.v(TAG, "onRestart() invoked");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.v(TAG, "onDestroy() invoked");
+        if (BuildConfig.DEBUG) Log.v(TAG, "onDestroy() invoked");
     }
 
 
---
+++ Yamba/src/main/java/com/twitter/twitteru/android/yamba/svc/YambaServiceHelper.java
@@ -0,0 +1,21 @@
+package com.twitter.twitteru.android.yamba.svc;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.twitter.twitteru.android.yamba.BuildConfig;
+
+public final class YambaServiceHelper {
+    public static final String TAG = "SVC_HELP";
+
+    private YambaServiceHelper() {}
+
+    public static void post(Context ctxt, String tweet) {
+        if (BuildConfig.DEBUG) { Log.d(TAG, "posting: " + tweet); }
+        Intent i = new Intent(YambaContract.Service.ACTION_EXECUTE);
+        i.putExtra(YambaContract.Service.PARAM_OP, YambaContract.Service.OP_POST);
+        i.putExtra(YambaContract.Service.PARAM_TWEET, tweet);
+        ctxt.startService(i);
+    }
+}
