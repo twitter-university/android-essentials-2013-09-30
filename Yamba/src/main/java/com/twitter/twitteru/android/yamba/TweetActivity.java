06412fc626624ca1af7cef2f7271fc1b79802906
Ken Jones <kennethj@twitter.com>
2013-10-02 21:44:05 -0400

    Removed unused code from TweetActivity

--- Yamba/src/main/java/com/twitter/twitteru/android/yamba/TweetActivity.java
+++ Yamba/src/main/java/com/twitter/twitteru/android/yamba/TweetActivity.java
@@ -20,8 +20,6 @@ public class TweetActivity extends Activity implements View.OnClickListener {
     public static final String TAG = "TweetActivity";
 
     EditText mEditTweet;
-    YambaClient mYambaClient;
-    Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +30,6 @@ public class TweetActivity extends Activity implements View.OnClickListener {
         mEditTweet = (EditText) findViewById(R.id.edit_tweet);
         Button buttonPost = (Button) findViewById(R.id.button_post);
         buttonPost.setOnClickListener(this);
-
-        // Initialize a reusable Toast
-        mToast = Toast.makeText(this, null, Toast.LENGTH_LONG);
-
-        mYambaClient = new YambaClient("student", "password");
     }
 
     @Override
@@ -61,75 +54,6 @@ public class TweetActivity extends Activity implements View.OnClickListener {
             // Post the tweet on a worker thread using an AsyncTask
             YambaServiceHelper.post(this, msg);
         }
-    }
-
-    /* Old AsyncTask implementation
-    class PostToTwitter extends AsyncTask<String, Void, Integer> {
-        @Override
-        protected Integer doInBackground(String... params) {
-            int result = R.string.post_tweet_fail;
-            if (params.length > 0) {
-                try {
-                    mYambaClient.postStatus(params[0]);
-                    result = R.string.post_tweet_success;
-                } catch (YambaClientException e) {
-                    Log.e(TAG, "Failed to post tweet", e);
-                }
-            }
-            return result;
-        }
-
-        @Override
-        protected void onPostExecute(Integer result) {
-            mToast.setText(result);
-            mToast.show();
-        }
-    }
-    */
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (BuildConfig.DEBUG) Log.v(TAG, "onStart() invoked");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (BuildConfig.DEBUG) Log.v(TAG, "onPause() invoked");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (BuildConfig.DEBUG) Log.v(TAG, "onResume() invoked");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (BuildConfig.DEBUG) Log.v(TAG, "onStop() invoked");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        if (BuildConfig.DEBUG) Log.v(TAG, "onRestart() invoked");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (BuildConfig.DEBUG) Log.v(TAG, "onDestroy() invoked");
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.tweet, menu);
-        return true;
     }
 
 }
